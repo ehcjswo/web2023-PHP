a33e290ee6197057feaf2737f7347cac2672871a
kimdohyeon <ehcjswo1@gmail.com>
2023-03-22 11:08:01 +0900

    2023.03.22

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
   $connect -> set_charset("utf-8");
 
   if(mysqli_connect_errno()){
-    echo "Database Connect False";
+    echo "Database Connect False!";
   } else {
-    echo "Database Connect True";
+    echo "Database Connect True!";
   }
 ?>
