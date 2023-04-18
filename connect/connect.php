f7cd176af42d1bf849e5fecfce3f356b45d884cf
KimDoHyeon <ehcjswo1@gmail.com>
2023-04-18 09:19:38 +0900

    04-18

--- connect/connect.php
+++ connect/connect.php
@@ -1,14 +1,13 @@
 <?php
-  $host = "localhost";
-  $user = "root";
-  $pw = "root";
-  $db = "sample";
-  $connect = new mysqli($host, $user, $pw, $db);
-  $connect -> set_charset("utf-8");
-
-  if(mysqli_connect_errno()){
-    echo "Database Connect False!";
-  } else {
-    // echo "Database Connect True!";
-  }
+    $host = "localhost";
+    $user = "root";
+    $pw = "root";
+    $db = "phpclass";
+    $connect = new mysqli($host,$user,$pw,$db);
+    $connect -> set_charset("utf-8");
+    if(mysqli_connect_errno()){
+        echo "Database Connect false";
+    } else {
+        echo "Database Connect True";
+    }
 ?>
