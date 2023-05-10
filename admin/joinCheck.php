1b4548f4cea49344b741f750c39e10235c258613
KimDoHyeon <ehcjswo1@gmail.com>
2023-05-10 12:37:59 +0900

    05-10

--- admin/joinCheck.php
+++ admin/joinCheck.php
@@ -1,23 +1,17 @@
 <?php
     include "../connect/connect.php";
 
-    // SELECT youEmail FROM adminMembers WHERE youEmail = {}
-
+    
     $type = $_POST['type'];
     $email = $_POST['youEmail'];
     $jsonResult = "bad";
-
-    if($type == "isEmailCheck"){
+    if( $type == "isEmailCheck"){
         $youEmail = $connect -> real_escape_string(trim($email));
-        $sql = "SELECT youEmail FROM adminMembers WHERE youEmail = '{$youEmail}'";
+        $sql = "SELECT adminEmail FROM adminMembers WHERE adminEmail = '{$youEmail}'";
     }
-
     $result = $connect -> query($sql);
-    
-
-    if($result -> num_rows == 0){
+    if( $result -> num_rows == 0 ){
         $jsonResult = "good";
     }
-
     echo json_encode(array("result" => $jsonResult));
 ?>
